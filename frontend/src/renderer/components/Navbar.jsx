7a82ab00e6901049c4a04657f4d445c1b7cd5dad
ouailkahi <ouail.kahi2004@gmail.com>
2024-06-04 18:01:44 +0100

    modified:   frontend/package-lock.json

--- frontend/src/renderer/components/Navbar.jsx
+++ frontend/src/renderer/components/Navbar.jsx
@@ -94,7 +94,7 @@ export default function Navbar({sidebarOut,handleSideBar}) {
 
     return (
         <React.Fragment>
-            <div className="ec-left-sidebar ec-bg-sidebar" style={{ background: 'white', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px' , zIndex:"1000"}}>
+            <div className="ec-left-sidebar ec-bg-sidebar" style={{ background: '#E0FBE2', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px' , zIndex:"1000"}}>
                 <div id="sidebar" className="sidebar ec-sidebar-footer">
                 <button id="sidebar-toggler" className="sidebar-toggle button-toggle" ></button>
 
@@ -103,11 +103,11 @@ export default function Navbar({sidebarOut,handleSideBar}) {
                             e.preventDefault();
                             toggleSidebar()
                         }}>
-                            <span className="ec-brand-name text-truncate" style={{ color: "BLACK", fontWeight: 'bold', fontFamily: 'serif' }}>WebCrea</span>
+                            <span className="ec-brand-name text-truncate" style={{ color: "rgb(61 176 68)", fontWeight: 'bold', fontFamily: 'serif',fontSize:"25px" }}>PHARMACIE</span>
                         </Link>
                     </div>
                     <div className="ec-navigation" data-simplebar>
-                        <ul className="nav sidebar-inner" id="sidebar-menu" style={{ color: "black" }}>
+                        <ul className="nav sidebar-inner" id="sidebar-menu" style={{ color: "rgb(61 176 68)" ,background:'#E0FBE2'}}>
                             {navbar.map((item, index) => (
                                 <li
                                     className={(item.subMenu.length > 0 ? subExpand === index ? "has-sub expand" : "has-sub" : "") + ((url === item.href || urlSub === item.title) ? " active" : "")}
@@ -126,16 +126,16 @@ export default function Navbar({sidebarOut,handleSideBar}) {
                                         }}
                                     >
                                         <i className={item.icon} style={{ color: "black" }}></i>
-                                        <span className="nav-text" style={{ color: "BLACK", fontWeight: 'bold' }}>{item.title}</span>
+                                        <span className="nav-text" style={{ color: "rgb(61 176 68)", fontWeight: 'bold' }}>{item.title}</span>
                                         {item.subMenu.length > 0 && <b className="caret" style={{ color: 'black' }}></b>}
                                     </Link>
                                     {item.subMenu.length > 0 && (
                                         <div className={subExpand === index ? "collapse show" : "collapse"}>
-                                            <ul className="sub-menu" id={item.title} data-parent="#sidebar-menu" style={{ color: "black" }}>
+                                            <ul className="sub-menu" id={item.title} data-parent="#sidebar-menu" style={{ color: "rgb(61 176 68)" ,background:"#E0FBE2"}}>
                                                 {item.subMenu.map((sub, indexSub) => (
-                                                    <li key={indexSub} className={(url === sub.href ? " active" : "")} style={{ color: "black" }} onClick={() => setUrlSub(item.title)}>
+                                                    <li key={indexSub} className={(url === sub.href ? " active" : "")} style={{ color: "rgb(61 176 68)",background:"#E0FBE2" }} onClick={() => setUrlSub(item.title)}>
                                                         <Link className="sidenav-item-link" to={sub.href}>
-                                                            <span className="nav-text" style={{ color: "black", fontWeight: 'bold' }}>{sub.title}</span>
+                                                            <span className="nav-text" style={{ color: "rgb(61 176 68)", fontWeight: 'bold' }}>{sub.title}</span>
                                                         </Link>
                                                     </li>
                                                 ))}
